89ca584256ef1afd281d36242a55cd8bf8a4bf7e
Van Tri <vtri2016@gmail.com>
2025-02-07 10:39:06 +0700

    done tutorial

--- packages/zoo/index.ts
+++ packages/zoo/index.ts
@@ -1,7 +1,8 @@
-import { getRandomAnimal } from "@tuskdesign/animals";
-import { getRandomName } from "@tuskdesign/names";
+import { getRandomAnimal } from '@tuskdesign/animals';
+import { getRandomName } from '@tuskdesign/names';
 
 const name = getRandomName();
 const animal = getRandomAnimal();
 
 console.log(`${name} the ${animal.name} says ${animal.sound}!`);
+
